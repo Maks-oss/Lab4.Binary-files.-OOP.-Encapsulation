ce2c3ba400ac7a3cdfc793c47aecc078fae7de65
Роберт Шевцов <55320224+RobertUA@users.noreply.github.com>
2020-05-05 20:25:09 +0300

    simpleout

--- LAB4 OOP/LAB4 OOP.cpp
+++ LAB4 OOP/LAB4 OOP.cpp
@@ -33,9 +33,9 @@ typedef struct
 
 typedef struct
 {
-	BYTE rgbBlue;
-	BYTE rgbGreen;
-	BYTE rgbRed;
+	BYTE rgbBlue=0x00;
+	BYTE rgbGreen=0x00;
+	BYTE rgbRed=0x00;
 } RGB;
 
 class Picture
@@ -46,7 +46,8 @@ private:
 public:
 	Picture(ifstream&file);
 	void makeBigger(ifstream&file,int n,ofstream&out);
-
+	void simpleout(ifstream& file, ofstream& out);
+	void inc(ifstream& file, int n, ofstream& out);
 };
 
 Picture::Picture(ifstream&file)
@@ -69,10 +70,9 @@ void Picture::makeBigger(ifstream&file,int n,ofstream&out)
 	//
 	out.write((char*)&bmphead, sizeof(bmphead));
 	out.write((char*)&bmpinfo, sizeof(bmpinfo));
-
-	for (int i = 0, biHeight = abs(bmpinfo.biHeight); i < biHeight; i++)
+	for (int i = 0; i <= height; i+=1)
 	{
-		for (int k = 0; k < bmpinfo.biWidth; k++)
+		for (int k = 0; k <= width; k+=1)
 		{
 			RGB triple;
 			file.read((char*)&triple, sizeof(RGB));
@@ -85,13 +85,42 @@ void Picture::makeBigger(ifstream&file,int n,ofstream&out)
 	}
 }
 
+void Picture::simpleout(ifstream& file, ofstream& out)
+{
+	int width = bmpinfo.biWidth;
+	int height = bmpinfo.biHeight;
+	int oldpadding = (4 - (width * sizeof(RGB)) % 4) % 4;
+	int padding = (4 - (bmpinfo.biWidth * sizeof(RGB)) % 4) % 4;
+
+	out.write((char*)&bmphead, sizeof(bmphead));
+	out.write((char*)&bmpinfo, sizeof(bmpinfo));
+
+	RGB** rgb = new RGB * [height+1];
+	for (int i = 0; i <= height; i += 1) rgb[i] = new RGB[width+1];
+
+	for (int i = 0; i <= height; i += 1)
+	{
+		for (int j = 0; j <= width; j += 1)
+		{
+			file.read((char*)&rgb[i][j], sizeof(RGB));
+		}
+	}
+	for (int i = 0; i <= height; i += 1)
+	{
+		for (int j = 0; j <= width; j += 1)
+		{
+			out.write((char*)&rgb[i][j], sizeof(RGB));
+		}
+	}
+}
 int main()
 {
-	
-	ifstream file("bmp.bmp", ios::binary);
+	ifstream file("d:\\bmpp.bmp", ios::binary);
 	ofstream out("d:\\HAHA.bmp", ios::binary);
 	Picture BMP(file);
-	BMP.makeBigger(file, 5, out);
+
+	BMP.simpleout(file, out);
+	//BMP.inc(file, 4, out);
 
 	return 0;
 }
