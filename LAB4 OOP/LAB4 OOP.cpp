c3355962b2b0f8eeef24bb19814ce60460fda31d
Роберт Шевцов <55320224+RobertUA@users.noreply.github.com>
2020-05-11 22:21:13 +0300

    kratno

--- LAB4 OOP/LAB4 OOP.cpp
+++ LAB4 OOP/LAB4 OOP.cpp
@@ -1,12 +1,14 @@
 ﻿#include<fstream>
 #include <iostream>
 
-using namespace std;
 #pragma pack(1)
+
+using namespace std;
 typedef uint8_t  BYTE;
 typedef uint32_t DWORD;
 typedef int32_t  LONG;
 typedef uint16_t WORD;
+
 typedef struct
 {
 	WORD    bfType;
@@ -44,10 +46,8 @@ private:
 	BITMAPFILEHEADER bmphead;
 	BITMAPINFOHEADER bmpinfo;
 public:
-	Picture(ifstream&file);
-	void makeBigger(ifstream&file,int n,ofstream&out);
-	void simpleout(ifstream& file, ofstream& out);
-	void inc(ifstream& file, int n, ofstream& out);
+	Picture(ifstream& file);
+	void makeBigger(ifstream& file, int n, ofstream&out);
 };
 
 Picture::Picture(ifstream&file)
@@ -56,71 +56,58 @@ Picture::Picture(ifstream&file)
 	file.read((char*)&bmpinfo, sizeof(bmpinfo));
 }
 
-void Picture::makeBigger(ifstream&file,int n,ofstream&out)
+void Picture::makeBigger(ifstream&file,int n, ofstream&out)
 {
 	int width = bmpinfo.biWidth;
 	int height = bmpinfo.biHeight;
 	//
 	bmpinfo.biWidth = bmpinfo.biWidth * n;
 	bmpinfo.biHeight = bmpinfo.biHeight * n;
-	int oldpadding = (4 - (width * sizeof(RGB)) % 4) % 4;
-	int padding = (4 - (bmpinfo.biWidth * sizeof(RGB)) % 4) % 4;
-	bmpinfo.biSizeImage = (bmpinfo.biWidth * sizeof(RGB) + padding) + abs(bmpinfo.biHeight);
+	//int oldpadding = (4 - (width * sizeof(RGB)) % 4) % 4;
+	int padding = (4 - (bmpinfo.biWidth * sizeof(RGB)) % 4)%4;
+	bmpinfo.biSizeImage = ((bmpinfo.biWidth + padding) * sizeof(RGB)) + bmpinfo.biHeight;
 	bmphead.bfSize = sizeof(BITMAPFILEHEADER) + sizeof(BITMAPINFOHEADER) + bmpinfo.biSizeImage;
 	//
 	out.write((char*)&bmphead, sizeof(bmphead));
 	out.write((char*)&bmpinfo, sizeof(bmpinfo));
-	for (int i = 0; i <= height; i+=1)
-	{
-		for (int k = 0; k <= width; k+=1)
-		{
-			RGB triple;
-			file.read((char*)&triple, sizeof(RGB));
 
-			for (int l = 0; l < n; l++)
+	RGB* rgb = new RGB [width];
+
+	cout << width << " " << height << "\n";
+	cout << bmpinfo.biWidth << " " << bmpinfo.biHeight << "\n";
+
+	for (int i = 0; i < height; i+=1)
+	{
+		for (int k = 0; k < width; k+=1)
+		{
+			file.read((char*)&rgb[k], sizeof(RGB));
+		}
+		for (int l = 0; l < n; l++)
+		{
+			int kk;
+			for (kk = 0; kk < width; kk += 1)
 			{
-				out.write((char*)&triple, sizeof(RGB));
+				for (int ll = 0; ll < n; ll++)
+				{
+					out.write((char*)&rgb[kk], sizeof(RGB));
+				}
+				for (int p = 0; p < padding; p++)
+				{
+					out.write("0x00", sizeof(RGB));
+				}
 			}
 		}
 	}
 }
 
-void Picture::simpleout(ifstream& file, ofstream& out)
-{
-	int width = bmpinfo.biWidth;
-	int height = bmpinfo.biHeight;
-	int oldpadding = (4 - (width * sizeof(RGB)) % 4) % 4;
-	int padding = (4 - (bmpinfo.biWidth * sizeof(RGB)) % 4) % 4;
-
-	out.write((char*)&bmphead, sizeof(bmphead));
-	out.write((char*)&bmpinfo, sizeof(bmpinfo));
-
-	RGB** rgb = new RGB * [height+1];
-	for (int i = 0; i <= height; i += 1) rgb[i] = new RGB[width+1];
-
-	for (int i = 0; i <= height; i += 1)
-	{
-		for (int j = 0; j <= width; j += 1)
-		{
-			file.read((char*)&rgb[i][j], sizeof(RGB));
-		}
-	}
-	for (int i = 0; i <= height; i += 1)
-	{
-		for (int j = 0; j <= width; j += 1)
-		{
-			out.write((char*)&rgb[i][j], sizeof(RGB));
-		}
-	}
-}
 int main()
 {
 	ifstream file("d:\\bmpp.bmp", ios::binary);
 	ofstream out("d:\\HAHA.bmp", ios::binary);
 	Picture BMP(file);
 
-	BMP.simpleout(file, out);
-	//BMP.inc(file, 4, out);
+	//BMP.simpleout(file, out);
+	BMP.makeBigger(file, 4, out);
 
 	return 0;
 }
